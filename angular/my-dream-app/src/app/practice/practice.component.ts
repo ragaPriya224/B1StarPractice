1f380b85f9cdd4f581698744883708608ccb5c6e
bbb <aaa@gmail.com>
2022-08-19 11:44:05 +0530

    2.template

--- angular/my-dream-app/src/app/practice/practice.component.ts
+++ angular/my-dream-app/src/app/practice/practice.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-practice]',
-  templateUrl: './practice.component.html',
+  template: '<div>inline works<div>', 
+  // templateUrl: './practice.component.html',
   styleUrls: ['./practice.component.css']
 })
 export class PracticeComponent implements OnInit {
