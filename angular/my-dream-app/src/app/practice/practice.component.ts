2a36dfe09460f72a6eaa855353c2f65b052ad4fb
bbb <aaa@gmail.com>
2022-08-19 11:40:19 +0530

    1.3 selector : used as attribute

--- angular/my-dream-app/src/app/practice/practice.component.ts
+++ angular/my-dream-app/src/app/practice/practice.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-practice',
+  selector: '[app-practice]',
   templateUrl: './practice.component.html',
   styleUrls: ['./practice.component.css']
 })
