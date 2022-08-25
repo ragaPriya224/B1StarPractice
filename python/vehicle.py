80f8fef7857e82429ad669e4435d1ce76a845fe6
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-08-25 13:42:20 +0530

    Add files via upload

---
+++ python/vehicle.py
@@ -0,0 +1,13 @@
+print("hey, welcome to python world")
+
+if 5 > 2:
+ print("it is greater")
+
+x = 5
+# y = "Hello, Team"
+print(type(x))  #int
+x = "user"
+print(type(x)) #str
+X = 22
+print(type(X))  #
+# python is case sensitive
